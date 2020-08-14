0c4daa09bb08cdfb88476035fff7cf714513a86e
Alexandre Bouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-08-14 18:37:06 -0300

    Update 4_Select_within_Select.sql

--- 4_Select_within_Select.sql
+++ 4_Select_within_Select.sql
@@ -1,26 +1,92 @@
--- 1. 
--- The example uses a WHERE clause to show the population of 'France'. 
--- Note that strings (pieces of text that are data) should be in 'single quotes';
+-- 1. List each country name where the population is larger than that of 'Russia'.
 
--- Modify it to show the population of Germany
-
-SELECT population FROM world
-  WHERE name = 'Germany';
-
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia');
+     
 -- 2.
--- Checking a list The word IN allows us to check if an item is in a list. 
--- The example shows the name and population for the countries 'Brazil', 'Russia', 'India' and 'China'.
+-- Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+ 
+SELECT name FROM world
+  WHERE continent = 'Europe' AND gdp/population >
+     (SELECT gdp/population FROM world
+      WHERE name= 'United Kingdom')  
+           
+-- 3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
 
--- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+SELECT name, continent FROM world
+WHERE continent = (SELECT continent FROM world
+WHERE name = 'Argentina')
+OR continent = (SELECT continent FROM world
+WHERE name = 'Australia')
+ORDER BY name;
 
-SELECT name, population FROM world
-  WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
---   3.
--- Which countries are not too small and not too big? BETWEEN allows range checking 
--- (range specified is inclusive of boundary values). The example below shows countries 
--- with an area of 250,000-300,000 sq. km. Modify it to show the country and the area for c
--- ountries with an area between 200,000 and 250,000.
+-- 4. Which country has a population that is more than Canada but less than Poland? Show the name and the population.
 
-SELECT name, area FROM world
-  WHERE area BETWEEN 200000 AND 250000
+SELECT name FROM world
+WHERE population > (SELECT population FROM world
+WHERE name = 'Canada')
+AND population < (SELECT population FROM world
+WHERE name = 'Poland');
+
+-- 5. Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
+-- Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+-- The format should be Name, Percentage for example:
+
+SELECT name, CONCAT(ROUND(population/(SELECT population FROM world WHERE name = 'Germany')*100),'%')FROM world
+WHERE continent = 'Europe';
+
+-- 6.
+-- Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+
+SELECT name FROM world WHERE gdp > ALL (SELECT gdp FROM world WHERE continent = 'Europe' AND gdp>0)
+
+-- 7. Find the largest country (by area) in each continent, show the continent, the name and the area:
+
+SELECT continent, name, area FROM world x
+WHERE area >= ALL
+(SELECT area FROM world y
+WHERE y.continent = x.continent);
+
+-- 8. List each continent and the name of the country that comes first alphabetically.
+
+
+SELECT continent, name FROM world x
+WHERE name <= ALL(
+SELECT name FROM world y
+WHERE y.continent=x.continent);
+
+
+-- 9. Find the continents where all countries have a population <= 25000000. 
+-- Then find the names of the countries associated with these continents. Show name, continent and population.
+
+
+SELECT name, continent, population FROM world
+WHERE continent NOT IN(
+SELECT continent
+FROM world
+WHERE population > 25000000);
+
+-- 10.
+-- Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
+
+SELECT name, continent FROM world x 
+WHERE population >= ALL(
+SELECT population * 3
+FROM world y 
+WHERE x.continent = y.continent AND x.name != y.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
